00a793a2eac1aea5a2a061160af72a3cfe99fa16
GLodi <lodi.giulio@gmail.com>
2020-09-11 21:31:25 +0200

    css fix

--- client/src/About.tsx
+++ client/src/About.tsx
@@ -3,33 +3,43 @@ import './About.css'
 
 function About() {
   return (
-    <div className="center">
-      <h1>
-        <p> literally just one canvas</p>
-      </h1>
-      <ul className="nobull">
-        <li>
-          ...but a bit special.
-          <br />
-          &nbsp;
-        </li>
-        <li>
-          This is essentially a copy of Reddit's 2017 r/place. Just tinier
-          (65x65) and designed to work on a 5$ Digital Ocean droplet.
-        </li>
-        <li>
-          All users connected to justonecanvas actually share the same canvas,
-          and the backend
-        </li>
-        <li>
-          updates all players of each other's moves thanks to WebSockets.
-          <br />
-          &nbsp;
-        </li>
+    <body className="about">
+      <div className="center">
+        <h1>
+          <p> literally just one canvas</p>
+        </h1>
+        <ul className="nobull">
+          <li>
+            ...but a bit special.
+            <br />
+            &nbsp;
+          </li>
+          <li>
+            This is essentially a copy of Reddit's 2017 r/place. Just tinier
+            (65x65) and designed to work on a 5$ Digital Ocean droplet.
+          </li>
+          <li>
+            All users connected to justonecanvas actually share the same canvas,
+            and the backend
+          </li>
+          <li>
+            updates all players of each other's moves thanks to WebSockets.
+            <br />
+            &nbsp;
+          </li>
 
-        <li>You can move once every second.</li>
-      </ul>
-    </div>
+          <li>
+            You can move once every second.
+            <br />
+            &nbsp;
+          </li>
+
+          <li>
+            <a href="https://giuliolodi.dev/how-i-built-justonecanvas">Blog</a>
+          </li>
+        </ul>
+      </div>
+    </body>
   )
 }
 
